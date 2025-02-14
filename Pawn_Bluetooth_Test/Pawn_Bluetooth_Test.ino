52822221cecc2e761f96cebd05793a01c3ce05b0
OperatorAlexCode <alexander.jr.karlsson@gmail.com>
2025-02-14 10:26:28 +0100

    Succesfully tested and got BLE working
    Created code where when button is connected to server and pressed it adds placeholder item to pawn

--- Pawn_Bluetooth_Test/Pawn_Bluetooth_Test.ino
+++ Pawn_Bluetooth_Test/Pawn_Bluetooth_Test.ino
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <U8g2lib.h>
+#include <ArduinoBLE.h>
+
+BLEService GamePawn("10e62b35-1ed8-4149-aeca-4df2e8b24132");
+
+BLEIntCharacteristic switchCharacteristic("10e62b35-1ed8-4149-aeca-4df2e8b24132", BLERead | BLEWrite);
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C
 u8g2(U8G2_R0,/*clock=*/22,/*data=*/21,U8X8_PIN_NONE);
@@ -23,29 +28,81 @@ int maxItems = 3;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial);
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("Player 1");
+  BLE.setAdvertisedService(GamePawn);
+
+  GamePawn.addCharacteristic(switchCharacteristic);
+
+  BLE.addService(GamePawn);
+
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
   u8g2.begin();
   u8g2.setFont(u8g2_font_7x14B_tr);
+  u8g2.clearBuffer();
+  u8g2.drawBox(2, 2, 100, 50);
+  u8g2.sendBuffer();
+
+  Serial.println("BLE Peripheral");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  u8g2.clearBuffer();
-  u8g2.drawStr(0, 10, "Player 1");
 
-  for (int x = 0; x < itemsInInventory; x++)
+    // listen for Bluetooth® Low Energy peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central)
+  {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+
+    // while the central is still connected to peripheral:
+    while (central.connected())
     {
-      int difference = 42-32;
-      int xPos = x * 42 + difference/2 ;
-      int yPos = 16+difference/2;
-      u8g2.drawXBMP(xPos, yPos, 32, 32, testIcon);
-    }
+      if (switchCharacteristic.written())
+      if (switchCharacteristic.value() == 1)
+      {
+        Increment();
+        switchCharacteristic.writeValue(0);
+      }
 
-  u8g2.sendBuffer();
-  delay(1000);
+      // if the remote device wrote to the characteristic,
+      u8g2.clearBuffer();
+      u8g2.drawStr(0, 10, "Player 1");
+      for (int x = 0; x < itemsInInventory; x++)
+        {
+          int difference = 42-32;
+          int xPos = x * 42 + difference/2 ;
+          int yPos = 16+difference/2;
+          u8g2.drawXBMP(xPos, yPos, 32, 32, testIcon);
+        }
+
+        u8g2.sendBuffer();
+        delay(500);
+    }
+  }
+
+  Serial.print(F("Disconnected from central: "));
+  Serial.println(central.address());
 }
 
-
-public void Increment()
+void Increment()
 {
     itemsInInventory++;
     itemsInInventory = itemsInInventory % (maxItems+1);
--- Server_Board_Test/Server_Board_Test.ino
+++ Server_Board_Test/Server_Board_Test.ino
@@ -3,6 +3,7 @@
 #include <SPI.h>
 #include <U8g2lib.h>
 #include <WiFiNINA.h>
+#include <ArduinoBLE.h>
 
 //U8G2_SH1107_SEEED_128X128_F_HW_I2C Screen(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
@@ -26,8 +27,22 @@ bool isPressed = false;
 
 void setup() {
   // put your setup code here, to run once:
-  //Screen.begin();
+  Serial.begin(9600);
+  while (!Serial);
+
   pinMode(LEDR, OUTPUT); 
+  pinMode(buttonPin, INPUT);
+
+  BLE.begin();
+  
+  Serial.println("Bluetooth® Low Energy Central - LED control");
+
+  // start scanning for peripherals
+  BLE.scanForUuid("10e62b35-1ed8-4149-aeca-4df2e8b24132");
+
+  //Screen.begin();
+
+  //BLE.begin();
 }
 
 void loop() {
@@ -37,15 +52,68 @@ void loop() {
   //Screen.sendBuffer();
   //delay(100);
 
-  buttonState = digitalRead(buttonPin);
+  BLEDevice pawn = BLE.available();
 
-  if (buttonState == LOW && !isPressed)
+  if (pawn)
+  {
+    Serial.print("Found ");
+    Serial.print(pawn.address());
+    Serial.print(" '");
+    Serial.print(pawn.localName());
+    Serial.print("' ");
+    Serial.print(pawn.advertisedServiceUuid());
+    Serial.println();
+    
+    if (pawn.localName() != "Player 1")
+      {
+        Serial.println("Can't find pawn"); 
+        return;
+      }
+    else
+      {
+        Serial.println("Found pawn"); 
+        BLE.stopScan();
+      }
+
+    if (pawn.connect())
+      {
+        Serial.println("Connected"); 
+        digitalWrite(LEDR, HIGH);
+      }
+    else
     {
-      digitalWrite(LEDR, (PinStatus)((++state)%2));
-      isPressed = true;
+      Serial.println("Can't connect"); 
+      return;
     }
-  else if (buttonState == HIGH)
+
+    if (pawn.discoverAttributes())
+    Serial.println("Attributes discovered");
+    else
     {
-      isPressed = false;
+      pawn.disconnect();
+      return;
     }
+
+    BLECharacteristic characteristic = pawn.characteristic("10e62b35-1ed8-4149-aeca-4df2e8b24132");
+
+    while (pawn.connected())
+    {
+      // Button toggle
+      buttonState = digitalRead(buttonPin);
+
+      if (buttonState == LOW && !isPressed)
+      {
+        //digitalWrite(LEDR, (PinStatus)((++state)%2));
+        isPressed = true;
+        characteristic.writeValue((byte)(0x01));
+      }
+      else if (buttonState == HIGH)
+      {
+        isPressed = false;
+      }
+    }
+  }
+
+  BLE.scanForUuid("10e62b35-1ed8-4149-aeca-4df2e8b24132");
+  digitalWrite(LEDR, LOW);
 }
