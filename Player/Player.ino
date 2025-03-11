0ed1f21538fb4582d6f6c15bb5336956857dc5f6
OperatorAlexCode <alexander.jr.karlsson@gmail.com>
2025-03-11 09:15:25 +0100

    Created new Player file for the pawn script
    Pawn display becomes blank when server connection is lost
    Pawn display updates only when necessary
    Created library to store icons

--- Player/Player.ino
+++ Player/Player.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <U8g2lib.h>
 #include <ArduinoBLE.h>
+#include "Icons.h"
 
 BLEService GamePawn("10e62b35-1ed8-4149-aeca-4df2e8b24132");
 
@@ -10,18 +11,8 @@ BLEIntCharacteristic switchCharacteristic("10e62b35-1ed8-4149-aeca-4df2e8b24132"
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C
 Screen(U8G2_R0,/*clock=*/22,/*data=*/21,U8X8_PIN_NONE);
 
-const uint8_t testIcon[] = {
-  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x03, 0x00, 0x00, 0xC0, 
-  0x03, 0x00, 0x00, 0xC0, 0x83, 0x07, 0xF8, 0xC7, 0x83, 0x07, 0xF8, 0xC7, 
-  0x03, 0x03, 0xC0, 0xC0, 0x03, 0x03, 0xC0, 0xC0, 0x03, 0x03, 0xC0, 0xC0, 
-  0x03, 0x03, 0xC0, 0xC0, 0x03, 0x03, 0xC0, 0xC0, 0x03, 0x03, 0xC0, 0xC0, 
-  0x83, 0x07, 0xC0, 0xC0, 0x83, 0x07, 0xC0, 0xC0, 0x03, 0x00, 0x00, 0xC0, 
-  0x03, 0x00, 0x00, 0xC0, 0x03, 0x00, 0x00, 0xC0, 0x03, 0x00, 0x00, 0xC0, 
-  0xC3, 0x0F, 0x1C, 0xCE, 0xC3, 0x0F, 0x3C, 0xCF, 0xC3, 0x00, 0xFC, 0xCF, 
-  0xC3, 0x00, 0xEC, 0xCD, 0xC3, 0x07, 0xCC, 0xCC, 0xC3, 0x07, 0xCC, 0xCC, 
-  0xC3, 0x00, 0x0C, 0xCC, 0xC3, 0x00, 0x0C, 0xCC, 0xC3, 0x0F, 0x0C, 0xCC, 
-  0xC3, 0x0F, 0x0C, 0xCC, 0x03, 0x00, 0x00, 0xC0, 0x03, 0x00, 0x00, 0xC0, 
-  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, };
+int Health = 5;
+int MaxHealth = 5;
 
 int itemsInInventory = 0;
 int maxItems = 3;
@@ -44,7 +35,6 @@ void setup() {
   GamePawn.addCharacteristic(switchCharacteristic);
 
   BLE.addService(GamePawn);
-
   switchCharacteristic.writeValue(0);
 
   // start advertising
@@ -56,7 +46,7 @@ void setup() {
   Screen.drawBox(2, 2, 100, 50);
   Screen.sendBuffer();
 
-  Serial.println("BLE Peripheral");
+  Serial.println("Trying to connect");
 }
 
 void loop() {
@@ -71,6 +61,7 @@ void loop() {
     Serial.print("Connected to central: ");
     // print the central's MAC address:
     Serial.println(central.address());
+    UpdateDisplay();
 
     // while the central is still connected to peripheral:
     while (central.connected())
@@ -80,33 +71,38 @@ void loop() {
       {
         Increment();
         switchCharacteristic.writeValue(0);
+        UpdateDisplay();
       }
 
       // if the remote device wrote to the characteristic,
-      Screen.clearBuffer();
-      Screen.drawStr(0, 10, "Player 1");
-      for (int x = 0; x < itemsInInventory; x++)
-        {
-          int difference = 42-32;
-          int xPos = x * 42 + difference/2 ;
-          int yPos = 16+difference/2;
-          Screen.drawXBMP(xPos, yPos, 32, 32, testIcon);
-        }
-
-        Screen.sendBuffer();
-        delay(500);
     }
-  }
 
-  Serial.print(F("Disconnected from central: "));
-  Serial.println(central.address());
-  Screen.clearBuffer();
-  Screen.drawBox(2, 2, 100, 64);
-  Screen.sendBuffer();
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+    Serial.println("Trying to Reconnect");
+    Screen.clearBuffer();
+    Screen.drawBox(2, 2, 100, 64);
+    Screen.sendBuffer();
+  }
 }
 
 void Increment()
 {
     itemsInInventory++;
     itemsInInventory = itemsInInventory % (maxItems+1);
+}
+
+void UpdateDisplay()
+{
+  Screen.clearBuffer();
+  Screen.drawStr(0, 10, "Player 1");
+  for (int x = 0; x < itemsInInventory; x++)
+  {
+    int difference = 42-32;
+    int xPos = x * 42 + difference/2;
+    int yPos = 16+difference/2;
+    Screen.drawXBMP(xPos, yPos, 32, 32, ItemPlaceholder);
+  }
+  
+  Screen.sendBuffer();
 }
