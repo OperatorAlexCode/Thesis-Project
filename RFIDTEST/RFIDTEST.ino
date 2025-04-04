726451070fc6bd37be2adef58ed15f3941ac59ab
emilpasse <emil@passe.se>
2025-04-04 13:06:00 +0200

    test updated

--- RFIDTEST/RFIDTEST.ino
+++ RFIDTEST/RFIDTEST.ino
@@ -1,7 +1,7 @@
 #include <MFRC522v2.h>
 #include <MFRC522DriverSPI.h>
 #include <SPI.h>
-//#include <MFRC522DriverI2C.h>
+#include <MFRC522DriverI2C.h>
 #include <MFRC522DriverPinSimple.h>
 #include <MFRC522Debug.h>
 #include <Arduino.h>
@@ -19,6 +19,7 @@ U8G2_SSD1306_128X64_NONAME_F_SW_I2C
 u8g2(U8G2_R0,/*clock=*/22,/*data=*/21,U8X8_PIN_NONE);
 
 void setup() {
+  
   u8g2.begin();
   Serial.begin(115200);  // Initialize serial communication
   while (!Serial);       // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
@@ -27,15 +28,19 @@ void setup() {
   SPI.begin(); // Init SPI bus
   MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);	// Show details of PCD - MFRC522 Card Reader details.
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  // nfc.setPassiveActivationRetries(0x9A);
+  // mfrc522.
 }
 
 void loop() {
+  
   while(readID()) {
     dump();
     print();
-    
+    //nfc.readPassiveTargetID()
     // delay(2000);
   }
+  
   
   
 }
@@ -70,32 +75,36 @@ void print()  {
     uidString += String(mfrc522.uid.uidByte[i], HEX);
   }
    Serial.println(uidString);
-   u8g2.drawStr(0,10,uidString.c_str());
+  //  u8g2.clearDisplay();
+  //  u8g2.setCursor(0,10);
+  u8g2.drawStr(0,10,uidString.c_str());
+  u8g2.sendBuffer();
+  //u8g2.updateDisplay();
+  //  u8g2.clearBuffer();
+  //  u8g2.firstPage();
+  // do {
+  //   /* all graphics commands have to appear within the loop body. */    
+  //   u8g2.setFont(u8g2_font_ncenB14_tr);
+  //   u8g2.drawStr(0,20,uidString.c_str());
+  // } while ( u8g2.nextPage() );
+   //u8g2.sendBuffer();
+   //u8g2.refreshDisplay();
+  
 }
 
 
 boolean readID()  {
-    //mfrc522.PCD_StopCrypto1();
-    //Check if a new tag is detected or not. If not return.
-    // if (!mfrc522.PICC_IsNewCardPresent()){
-    //   Serial.println("PICC_IsNewCardPresent");
-    //   delay(2000);
-    //   return false;
-    // }
-    // //Check if a new tag is readable or not. If not return.
-    // if (!mfrc522.PICC_ReadCardSerial()){
-    //   Serial.println("PICC_ReadCardSerial");
-    //   delay(2000);
-    //   return false;
-    // }
 
+  byte bufferATQA[2];
+byte bufferSize = sizeof(bufferATQA);
+Serial.print("test");
+mfrc522.PICC_WakeupA(bufferATQA, &bufferSize);
     if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
-    return false;
+      return false;
   }
-  // u8g2.clearDisplay();
   // delay(500);
-  // u8g2.drawStr(0,10,"test");
-  // u8g2.sendBuffer();
+  //u8g2.drawStr(0,10,uidString);
+  
   // delay(500);
     
     mfrc522.PICC_HaltA(); // Stop reading
